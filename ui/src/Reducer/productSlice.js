3c8b9bb4dd355270b79d27fbe3017eb6a29e1731
Nahid Ul Kabir <rafi.eee.1999@gmail.com>
2023-10-15 23:44:28 +0600

    Bug FIX : same product canot be added multiple time in cart

--- ui/src/Reducer/productSlice.js
+++ ui/src/Reducer/productSlice.js
@@ -17,7 +17,10 @@ const ProuctSlice = createSlice({
         state.product = action.payload
        },
        addToCart:(state , action) => {
-         state.cart.push(state.product.find( e => e._id === action.payload ))
+       
+        if(!state.cart.find(e => e._id === action.payload)){
+          state.cart.push(state.product.find( e => e._id === action.payload ))
+        }
        },
        removeToCart:(state , action) => {
        const newcart = state.cart.filter( e => e._id !== action.payload)
